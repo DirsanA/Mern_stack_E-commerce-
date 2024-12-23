dbad667589ece00a805858c68d64a0e3326c66b4
DirsanA <dirsanantehun739@gmail.com>
2024-12-23 04:14:20 -0800

    Mongodb conncetion is setuped

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,9 @@
 import express from "express";
-import dotenv from "dotenv"; //I install dotenv package to acess dot env variables
+import { connectDB } from "./config/db.js";
 const app = express();
 const PORT = 5000;
-
+import dotenv from "dotenv"; //I install dotenv package to acess dot env variables
 dotenv.config();
-
 // The 'get' method defines a route for HTTP GET requests.
 // It takes two parameters:
 // 1. The route path ("/" in this case).
@@ -13,8 +12,10 @@ app.get("/products", function (req, res) {
   res.send("server is started");
 });
 
+console.log(process.env.MONGO_URI);
 // Start the server and listen on the specified port.
 // The callback function runs when the server starts successfully.
 app.listen(PORT, function () {
+  connectDB();
   console.log(`The server is listening at port ${PORT}`);
 });

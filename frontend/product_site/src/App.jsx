9d054da8b4e5fbd1864782ebe685daa16f3400ca
DirsanA <dirsanantehun739@gmail.com>
2024-12-24 12:16:24 -0800

    navbar componet is created

--- frontend/product_site/src/App.jsx
+++ frontend/product_site/src/App.jsx
@@ -1,11 +1,11 @@
-import { Box } from "@chakra-ui/react";
+import { Box, useColorModeValue } from "@chakra-ui/react";
 import HomePage from "./pages/HomePage";
 import CreatePage from "./pages/CreatePage";
 import Navbar from "./componets/Navbar";
 import { Route, Routes } from "react-router-dom";
 const App = () => {
   return (
-    <Box minH={"100vh"}>
+    <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
--- frontend/product_site/src/componets/Navbar.jsx
+++ frontend/product_site/src/componets/Navbar.jsx
@@ -1,5 +1,59 @@
+import {
+  Button,
+  Container,
+  Flex,
+  Text,
+  HStack,
+  Link as ChakraLink,
+  Icon,
+  useColorMode,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { Link as RouterLink } from "react-router-dom";
+import { CgAddR } from "react-icons/cg";
+import { FaSun, FaMoon } from "react-icons/fa";
+
 const Navbar = () => {
-  return <div></div>;
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <Container maxW={"1140px"}>
+      <Flex
+        h={16}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        flexDir={{
+          base: "column",
+          sm: "row",
+        }}
+      >
+        {/* Logo */}
+        <Text
+          fontSize={{ base: 22, sm: "28" }}
+          fontWeight={"bold"}
+          textAlign={"center"}
+          textTransform={"uppercase"}
+          bgGradient={"linear(to-r, cyan.400, blue.500)"}
+          bgClip={"text"}
+        >
+          <ChakraLink as={RouterLink} to={"/"}>
+            Product Store 🛍️
+          </ChakraLink>
+        </Text>
+
+        {/* Add Product Button */}
+        <HStack spacing={2} alignItems={"center"}>
+          <ChakraLink as={RouterLink} to={"/create"}>
+            <Button p={4}>
+              <Icon as={CgAddR} fontSize={20} />
+            </Button>
+          </ChakraLink>
+          <Button onClick={toggleColorMode}>
+            {colorMode === "light" ? <FaMoon /> : <FaSun />}
+          </Button>
+        </HStack>
+      </Flex>
+    </Container>
+  );
 };
 
 export default Navbar;
